858289a5385405e3e83ca335d07e760a9bb4c910
Soila Pertet <nicolepertet@gmail.com>
2026-03-03 11:45:28 -0700

    Connect routes to server.js and include Browse routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import userRoutes from './routes/userRoutes.js';
+import transactionRoutes from './routes/transactionRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
-});
+// Requests to /user will be directed to the userRoutes.js module
+app.use("/user", userRoutes);
+
+// Requests to /transactions will be directed to the transactionRoutes.js module 
+app.use("/transactions", transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 
