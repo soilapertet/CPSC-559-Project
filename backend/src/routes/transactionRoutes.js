39024d9488b6d40bcbc8682fd853448df4f19082
Soila Pertet <nicolepertet@gmail.com>
2026-03-03 10:55:51 -0700

    Add browse operations and user routers

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,14 +1,22 @@
 // Defines endpoints for searching, borrowing, and returning books.
 import express from 'express';
 
-// Import the controller modules into the transactionRoutes.js file
-import * as borrowController from '../controllers/borrowController';
-import * as returnController from '../controllers/returnController';
-import * as searchController from '../controllers/searchController';
+// Import the transaction controller modules into the transactionRoutes.js file
+import * as borrowController from '../controllers/borrowController.js';
+import * as browseController from '../controllers/browseController.js';
+import * as returnController from '../controllers/returnController.js';
+import * as searchController from '../controllers/searchController.js';
 
 const router = express.Router();
 
 // Define routes
+
+// State-changing operations (Write Operations)
 router.post('/borrow', borrowController.borrowBooks);
 router.post('/return', returnController.returnBooks);
+
+// Read-only operations
+router.get('/browse', browseController.getBooks);
 router.get('/search', searchController.findBooks);
+
+export default router;
