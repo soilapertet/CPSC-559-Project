f45d05896243f0107a79dbb42484b248255580be
Soila Pertet <nicolepertet@gmail.com>
2026-03-03 10:41:00 -0700

    Define routes for transaction operations

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,1 +1,14 @@
 // Defines endpoints for searching, borrowing, and returning books.
+import express from 'express';
+
+// Import the controller modules into the transactionRoutes.js file
+import * as borrowController from '../controllers/borrowController';
+import * as returnController from '../controllers/returnController';
+import * as searchController from '../controllers/searchController';
+
+const router = express.Router();
+
+// Define routes
+router.post('/borrow', borrowController.borrowBooks);
+router.post('/return', returnController.returnBooks);
+router.get('/search', searchController.findBooks);
