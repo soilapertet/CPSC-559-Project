d8d7ddfb0e1f9d9b5d01f1acf076624f7c5bde6d
Soila Pertet <nicolepertet@gmail.com>
2026-03-03 21:45:00 -0700

    Define methods to get books from the online library

--- backend/src/controllers/booksController.js
+++ backend/src/controllers/booksController.js
@@ -1,1 +1,48 @@
-// Handles logice for browsing all books within database
+// Handles logic for browsing all books within database
+
+// Import the Book model
+import Book from '../models/Book.js';
+
+// Define a function to fetch a book by its id
+export const getBookById = async(req, res) => {
+
+    // Extract the book id from the HTTP request
+    const bookId = req.params.id;
+
+    try {
+        const book = await Book.findById(bookId);
+
+        // Return an error if the book is not found using the provided book id
+        if(!book) {
+            return res.status(404).json({ error : "Book not found."});
+        }
+
+        // Else, return result
+        res.json(book);
+    } catch(err) {
+        res.status(500).json({ error : "An error occurred while fetching book."})
+    }
+}
+
+// Define a function to return all the books within the online library
+export const getBooks = async(req, res) => {
+
+    // Define the query body to query the MongoDB database
+    const query = {};
+
+    // Extract genre(s) from HTTP request, if present
+    const { genre } = req.query;
+
+    if(genre) {
+        query.genres = { $in : [genre] };
+    } 
+
+    try {
+        const books = await Book.find(query);
+        res.json(books)
+    } catch (err) {
+        res
+            .status(500)
+            .json({ error : "An error occurred while fetching the books."})
+    }
+}
