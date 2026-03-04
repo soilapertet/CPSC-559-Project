d940751473abe4ad2690648ea9cb3ac866d721aa
Soila Pertet <nicolepertet@gmail.com>
2026-03-03 18:40:25 -0700

    Define the schemas for the database (subject to change)

---
+++ backend/src/models/Book.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+// Define the schema of a Book instance and specify the required fields
+// totalCopies and availableCopies have a min value of 0 to avoid negative values 
+// isbn variable will store unique values
+// keywords and subjects will store an array of strings
+const bookSchema = new Schema({
+    title : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    author : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    isbn : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
+    totalCopies : {
+        type : Number,
+        required : true,
+        min : 0
+    },
+    availableCopies : {
+        type : Number,
+        required : true,
+        min : 0
+    },
+    keywords : {
+        type : [String],
+        default : []
+    },
+    subjects : {
+        type : [String],
+        default : []
+    },
+},
+    { timestamps : true }
+);
+
+bookSchema.index({ subjects : 1 });
+bookSchema.index({ keywords : 1 });
+bookSchema.index({ author : 1 });
+
+bookSchema.index({ 
+    title : "text",
+    author : "text",
+    keywords : "text", 
+    subjects : "text" 
+});
+
+const Book = model('Book', bookSchema);
+export default Book;
---
+++ backend/src/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+// Define the schema of a User instance and specify the required fields
+// userName and email variables will store to unique values
+const userSchema = new Schema({
+    firstName : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    lastName : { 
+        type : String,
+        required : true,
+        trim : true
+    },
+    userName : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
+    email : {
+        type : String, 
+        required : true,
+        unique : true,
+        trim : true,
+        lowercase : true
+    }
+},
+    { timestamps : true }
+);
+
+const User = model('User', userSchema);
+export default User;
