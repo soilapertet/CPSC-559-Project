61acec9aaf55de227b1088ab2a10060a5b4aa7f8
Jjychu <74945751+Jjychu@users.noreply.github.com>
2026-03-04 19:23:25 -0700

    Changed User.js back to userName instead of libraryId

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,11 +3,6 @@ import mongoose from "mongoose";
 // Define the schema of a User instance and specify the required fields
 // userName and email variables will store to unique values
 const userSchema = new mongoose.Schema({
-    libraryId : {
-        type : String,
-        required : true,
-        unique : true,
-    },
     firstName : {
         type : String,
         required : true,
@@ -18,19 +13,19 @@ const userSchema = new mongoose.Schema({
         required : true,
         trim : true
     },
+    userName : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
     email : {
         type : String, 
         required : true,
         unique : true,
         trim : true,
         lowercase : true
-    },
-    borrowedBooks : [
-        {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "Book"
-        }
-    ]
+    }
 },
     { timestamps : true }
 );
