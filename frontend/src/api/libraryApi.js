6a89059405c59f61643e3b595efbbddb7bd601d1
Soila Pertet <nicolepertet@gmail.com>
2026-03-19 22:41:04 -0600

    Implement load balancing in library api

--- frontend/src/api/libraryApi.js
+++ frontend/src/api/libraryApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// Create an axios instance for Leader - write or read operatiosn are directed to the leader
 const leaderApi = axios.create({
   // here I hardcode the leader URL,
   // but later it should be dynamic based on the response from election
@@ -11,7 +12,7 @@ const leaderApi = axios.create({
   timeout: 10000,
 })
 
-// all read operations are directed to follower 1
+// Create an axios instance for follower 1 - read operations are directed to follower 1
 const follower1Api = axios.create({
   baseURL: import.meta.env.FOLLOWER1_URL || 'http://localhost:3002/',
   headers: {
@@ -19,6 +20,23 @@ const follower1Api = axios.create({
   },
   timeout: 10000,
 })
+
+// Create an axios instance for follower 2 - read operations are directed to follower 2
+const follower2Api = axios.create({
+  baseURL: import.meta.env.FOLLOWER2_URL || 'http://localhost:3003/',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  timeout: 10000,
+})
+
+// Distribute read requests evenly between follower nodes (Load balancing)
+const apis = [follower1Api, follower2Api];
+let index = 0;
+
+function getApi() {
+  return apis[index++ % apis.length]
+}
 
 // Run server by: 
 // Leader: Start-Process powershell -ArgumentList '-NoExit', '$env:PORT=3001; npm run dev'
@@ -28,19 +46,25 @@ const follower1Api = axios.create({
 // ─── API functions ─────────────────────────────────────────────────────────────
 
 export async function getAllBooks(params = {}) {
+
+  const follower = getApi();
+
   if (params && Object.keys(params).length > 0) {
     const key = Object.keys(params)[0];
     try {
-      const response = await follower1Api.get('/books/search', {
+      const response = await follower.get('/books/search', {
         params: { [key]: params[key] }
       });
       return { data: { success: true, data: response.data } }
-    
-    } catch (timeoutError) {
-      // try the leader if follower 1 is down
+
+    } catch (err) {
+      if (err.code === 'ECONNABORTED') {
+        console.warn('Follower timed out, falling back to leader...');
+      }
+      // try the leader if followers are down
       try {
         const response = await leaderApi.get('/books/search', {
-          params: { [key] : params[key] }
+          params: { [key]: params[key] }
         });
         return { data: { success: true, data: response.data } }
       } catch (error) {
@@ -48,13 +72,16 @@ export async function getAllBooks(params = {}) {
       }
     }
   }
-  else{
+  else {
     try {
-      const response = await follower1Api.get('/books');
+      const response = await follower.get('/books');
       return { data: { success: true, data: response.data } }
-    
-    } catch (timeoutError) {
-      // try the leader if follower 1 is down
+
+    } catch (err) {
+      if (err.code === 'ECONNABORTED') {
+        console.warn('Follower timed out, falling back to leader...');
+      }
+      // try the leader if followers are down
       try {
         const response = await leaderApi.get('/books');
         return { data: { success: true, data: response.data } }
@@ -67,22 +94,28 @@ export async function getAllBooks(params = {}) {
 }
 
 export async function searchBooks(q, type = 'Keyword') {
+
+  const follower = getApi();
+
   try {
     let response;
     if (type === 'id') {
-      response = await follower1Api.get(`/books/${q}`);
+      response = await follower.get(`/books/${q}`);
       // make sure the returned data is in array format
       // if response is a single book object, wrap it in an array
       return { data: { success: true, data: response.data ? [response.data] : [] } }
     }
     else {
-      response = await follower1Api.get('/books/search', {
+      response = await follower.get('/books/search', {
         params: { [type.toLowerCase()]: q }
       });
       return { data: { success: true, data: response.data } }
     }
-  } catch (timeoutError) {
-    // try the leader if follower 1 is down
+  } catch (err) {
+    if (err.code === 'ECONNABORTED') {
+      console.warn('Follower timed out, falling back to leader...');
+    }
+    // try the leader if followers are down
     try {
       let response;
       if (type === 'id') {
@@ -91,7 +124,7 @@ export async function searchBooks(q, type = 'Keyword') {
       }
       else {
         response = await leaderApi.get('/books/search', {
-        params: { [type.toLowerCase()]: q }
+          params: { [type.toLowerCase()]: q }
         });
         return { data: { success: true, data: response.data } }
       }
@@ -105,7 +138,7 @@ export async function searchBooks(q, type = 'Keyword') {
 export async function loginUser(identifier) {
   // try the leader if follower 1 is down
   const response = await leaderApi.post('/books/user/login', { identifier })
-  return response.data 
+  return response.data
 }
 
 export async function registerUser(firstName, lastName, userName, email) {
@@ -122,20 +155,33 @@ export async function returnBook(userId, bookId) {
 }
 
 export async function getActiveBorrows(userId) {
+
+  const follower = getApi();
+
   try {
-    return await follower1Api.get(`/borrow/active/${userId}`)
+    return await follower.get(`/borrow/active/${userId}`)
   }
-  catch (timeoutError) {
+  catch (err) {
+    if (err.code === 'ECONNABORTED') {
+      console.warn('Follower timed out, falling back to leader...');
+    }
     // try the leader if follower 1 is down
     return await leaderApi.get(`/borrow/active/${userId}`)
   }
 }
 
 export async function getBorrowHistory(userId) {
+
+  const follower = getApi();
+
   try {
-    return await follower1Api.get(`/borrow/history/${userId}`)
+    return await follower.get(`/borrow/history/${userId}`)
   }
-  catch (timeoutError) {
+  catch (err) {
+    if (err.code === 'ECONNABORTED') {
+      console.warn('Follower timed out, falling back to leader...');
+    }
+
     return await leaderApi.get(`/borrow/history/${userId}`)
   }
 }
