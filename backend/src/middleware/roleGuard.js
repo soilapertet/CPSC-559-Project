d62dee9c38a4351211d57a02e991af1c16a16d46
Soila Pertet <nicolepertet@gmail.com>
2026-03-20 05:41:43 -0600

    Add a guard to direct write operations to leader node only

--- backend/src/middleware/roleGuard.js
+++ backend/src/middleware/roleGuard.js
@@ -1,1 +1,12 @@
 // Logic to block writes to Follower nodes
+import { config } from "../config/config";
+
+export const isLeader = (req, res, next) => {
+    
+    // Checks if the write operation is being directed to the leader node
+    if(config.role != 'leader') {
+        return res.status(403).json({ error: 'Write operations must be directed to leader node.'})
+    }
+
+    next()
+}
--- backend/src/routes/booksRoutes.js
+++ backend/src/routes/booksRoutes.js
@@ -1,6 +1,7 @@
 // Handle general operations such as displaying all books, filtering by category, getting book 
 // details etc.
 import express from 'express';
+import { isLeader } from '../middleware/roleGuard.js';
 
 import * as booksController from '../controllers/booksController.js';
 import * as borrowController from '../controllers/borrowController.js';
@@ -10,10 +11,12 @@ import * as searchController from '../controllers/searchController.js';
 const router = express.Router();
 
 // State-changing operations (Write Operations)
-router.post('/borrow', borrowController.borrowBook);
-router.post('/return', returnController.returnBook);
+// Add a guard between route and route handler to ensure only write operations are directed to the leader
+router.post('/borrow', isLeader, borrowController.borrowBook);
+router.post('/return', isLeader, returnController.returnBook);
 
 // Read-only operations
+// No guard since any node can handle read operations
 router.get('/search', searchController.searchBooks);
 
 // Get a book based on the provided book id
