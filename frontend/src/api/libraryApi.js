ceccf329ae0abc19de7a3072160416d423dccd42
Soila Pertet <nicolepertet@gmail.com>
2026-03-27 18:36:14 -0600

    Add commands to crash invidividual nodes in the terminal

--- frontend/src/api/libraryApi.js
+++ frontend/src/api/libraryApi.js
@@ -6,6 +6,7 @@ export const ALL_NODE_URLS = [
   'http://localhost:3002/',
   'http://localhost:3003/',
   'http://localhost:3004/',
+  'http://localhost:3005',
 ]
 
 const leaderApi = axios.create({
