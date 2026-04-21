5f33400a1f5ade5941123eecf5f9dcfa242e1ec6
Soila Pertet <nicolepertet@gmail.com>
2026-04-21 10:55:21 -0600

    fix bug .env file

--- frontend/src/api/libraryApi.js
+++ frontend/src/api/libraryApi.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 // All known node URLs — used to poll for new leader during election
 export const ALL_NODE_URLS = [
-  'http://100.122.74.20:3001',
+  'http://100.116.122.12:3001',
   'http://100.123.38.106:3002',
-  'http://100.116.122.12:3003'
+  'http://100.122.74.20:3003',
 ]
 
 // Generate a dynamic dictionary where key = URL and value = axios instance
